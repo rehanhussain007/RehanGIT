161d4cc80a5e9d37d38e348606f877e30387ba0b
rehanhussain007 <amy.rehan@gmail.com>
2019-05-15 13:07:24 +0530

    // commit by razor added Registration vc and label

---
+++ RehanGIT/RegistrationVC.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationVC.swift
+//  RehanGIT
+//
+//  Created by TFG-MAC on 15/05/19.
+//  Copyright © 2019 TFG-MAC. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

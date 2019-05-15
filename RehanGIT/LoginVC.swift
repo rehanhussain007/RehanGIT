bd49ea1952ac01369c78cffae3ce7dc9bd02682d
rehanhussain007 <amy.rehan@gmail.com>
2019-05-15 12:55:00 +0530

    // second commit from login branch

---
+++ RehanGIT/LoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginVC.swift
+//  RehanGIT
+//
+//  Created by TFG-MAC on 15/05/19.
+//  Copyright © 2019 TFG-MAC. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hii I am from login brnach with login vc.")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
